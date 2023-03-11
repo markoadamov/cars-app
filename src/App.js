8f897d638fe9c78a15a916ed7cf40c2726c50704
Marko Adamov <markoadamov.zr@gmail.com>
2023-03-11 20:26:43 +0100

    1. Zadatak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
+import AppCars from "./pages/AppCars";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <nav className="navigation">
+            <div>
+              <Link to="/cars">Cars </Link>
+            </div>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/cars" />
+          </Route>
+          <Route exact path="/cars">
+            <AppCars />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
